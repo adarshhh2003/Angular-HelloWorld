0cfe3fdaa76fb292accb794e20494ca29bed7bc2
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-03-06 15:06:50 +0530

    [Adarsh] Add. imgUrl in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'HelloWorld';
+  imgUrl = "assets/BridgeLabzLogo.jpg";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";

9ad3244773a6a653f4c37e4e6e2659e0f6c52480
adarshhh2003 <119410264+adarshhh2003@users.noreply.github.com>
2025-03-06 15:21:15 +0530

    [Adarsh] Add. url and onclick event handle to launch bridgelabz site in new tab

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,14 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'HelloWorld';
   imgUrl = "assets/BridgeLabzLogo.jpg";
+  url = "https://www.bridgelabz.com";
 
   ngOnInit(): void {
     this.title = "Hello from BridgeLabz.";
+  }
+
+  onClick(event: Event) {
+    console.log("Save button is clicked!", event);
+    window.open(this.url, "_blank");
   }
 }
